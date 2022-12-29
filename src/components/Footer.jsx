77526411eb9b846e3cdccaa94debdf9463910602
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-12-29 13:32:44 +0100

    amend footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="fixed-bottom">
-      <div className="text-center text-bg-dark p-3 ">© 2022 Greg Store</div>
-    </footer>
+    <div className="text-bg-dark">
+      <footer className="container py-1 mt-4">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          <li className="nav-item">
+            <Link to="/regulations" className="nav-link px-2 text-bg-dark">
+              Regulations
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/privacy-policy" className="nav-link px-2 text-bg-dark">
+              Privacy Policy
+            </Link>
+          </li>
+        </ul>
+        <p className="text-center">© 2022 Greg Store</p>
+      </footer>
+    </div>
   );
 };
 
